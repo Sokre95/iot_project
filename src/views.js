d98d5023224ada1e39ff70ecd83429a43293ac6b
PetarP <pp48578@fer.hr>
2019-06-13 13:44:57 +0200

    Added function for generating the modal DOM

--- src/views.js
+++ src/views.js
@@ -53,9 +53,63 @@ const generateSensorDOM = (sensor) => {
   buttonEl.innerHTML = 'Detalji'
   buttonEl.setAttribute('data-toggle', 'modal')
   buttonEl.setAttribute('data-target', '#exampleModalLong')
+  buttonEl.addEventListener('click', () => {
+    document.querySelector('#test-id').innerHTML = ''
+    document.querySelector('#test-id').appendChild(generateModalDOM(sensor))
+  })
   cardFooterEl.appendChild(buttonEl)
 
   return container
 }
+
+/* <div class="modal-content">
+  <div class="modal-header">
+    <h5 class="modal-title" id="exampleModalLongTitle">Taj i taj senzor</h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    Razne informacije o senzoru + stupicasti grafovi
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Zatvori</button>
+  </div>
+</div> */
+
+const generateModalDOM = (sensor) => {
+  const container = document.createElement('div')
+  const modalHeaderEl = document.createElement('div')
+  const modalTitleEl = document.createElement('h5')
+  const modalBodyEl = document.createElement('div')
+  const modalFooterEl = document.createElement('div')
+  const closeButton = document.createElement('button')
+
+  container.classList.add('modal-content')
+
+  modalHeaderEl.classList.add('modal-header')
+  container.appendChild(modalHeaderEl)
+
+  modalTitleEl.classList.add('modal-title')
+  modalTitleEl.textContent = sensor.name
+  modalHeaderEl.appendChild(modalTitleEl)
+
+  modalBodyEl.classList.add('modal-body')
+  modalBodyEl.textContent = 'Razne informacije o senzoru'
+  modalBodyEl.textContent += sensor.state
+  container.appendChild(modalBodyEl)
+
+  modalFooterEl.classList.add('modal-footer')
+  container.appendChild(modalFooterEl)
+
+  closeButton.classList.add('btn', 'btn-secondary')
+  closeButton.type = 'button'
+  closeButton.setAttribute('data-dismiss', 'modal')
+  closeButton.textContent = 'Zatvori'
+  modalFooterEl.appendChild(closeButton)
+
+  return container
+}
+
 
 export { generateSensorDOM }
